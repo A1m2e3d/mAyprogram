af0c0d4cbb1a9e644e8861a4fbdcc0744052cbab
ahmed <hassaniahmed9090@gmail.com>
2026-04-07 23:53:44 +0300

    index of an element in array

---
+++ src/Findindex.java
@@ -0,0 +1,14 @@
+public class Findindex {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+        int target = 30;
+        int index = -1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println(index);
+    }
+}
