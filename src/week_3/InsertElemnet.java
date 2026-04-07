559a07a09b15c615ad76dd7ad6c6f9ea7f5af94b
ahmed <hassaniahmed9090@gmail.com>
2026-04-07 14:16:40 +0300

    FINISHED Exercse 4

---
+++ src/week_3/InsertElemnet.java
@@ -0,0 +1,21 @@
+package week_3;
+
+public class InsertElemnet {
+
+    public static void main(String[] args) {
+        int[] mySum = { 67, 56, 78, 87 };
+        int move = 9;
+
+        int[] movesum = new int[mySum.length + 1];
+
+        for (int i = 0; i < movesum.length; i++) {
+            if (i == 0) {
+                movesum[i] = move;
+            } else {
+                movesum[i] = mySum[i - 1];
+            }
+
+            System.out.println("At index " + i + " value is: " + movesum[i]);
+        }
+    }
+}
