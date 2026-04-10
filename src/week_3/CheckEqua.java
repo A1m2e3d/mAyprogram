89cc63af3bc00419a2ca02de6a85b401ecdbfb2d
ahmed <hassaniahmed9090@gmail.com>
2026-04-10 14:10:47 +0300

    CheckEqua

---
+++ src/week_3/CheckEqua.java
@@ -0,0 +1,11 @@
+package week_3;
+
+public class CheckEqua {
+        public static void main(String[] args) {
+            String s1 = "Java";
+            String s2 = "Java";
+
+            boolean isEqual = s1.equals(s2);
+            System.out.println("Are strings equal? " + isEqual);
+        }
+    }
